83431e2ec184577a03e7c1ec0934cec0d45c3aed
bnockles <bnockles@100.64.129.66>
2018-01-16 22:35:47 -0500

    Created documentation of nearly all public methods for all GUIApplication files

--- Learning Github/src/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-
-public class HelloWorld {
-
-	public HelloWorld() {
-		// I am using Github for the first time! - Ben
-		//Now I am editing the file on Github!
-	}
-
-}
---
+++ Learning Github/src/Task.java
@@ -0,0 +1,31 @@
+
+/**
+ * 
+ * @author WRITE YOUR NAME HERE
+ * 
+ * Complete one of the following tasks. 
+ * Please note the difference in awarded credit. You may not consult any notes:
+
+TASK A (Worth 80%)
+A window appears on screen on which there are two buttons: a Button that says "Lose"
+ and a button that says "Win". If the user clicks the Button that says "Lose", 
+ text appears that says "You lose." Furthermore, clicking either Button will not
+  do anything (You cannot click the "Win" button after clicking the "Lose" Button)
+   Alternatively, if the user clicks the "Win" button, the text reads "You win." 
+   and both Buttons are disabled.
+
+TASK (Worth 100%)
+A window appears on screen on which there is one Button and text that says 
+"Score: 0". The Button says "Ready". When the user clicks the Button, a countdown 
+appears: "3...2...1... Go!" After the words "Go" appears, the Text on the Button 
+says "Click Me!". A clock shows that there are five seconds. During this time, 
+(and only during these five seconds) each time the user clicks the button, the Score 
+increases by 1. After 5 seconds ends, the text on the Button says "Done". Clicking 
+the button does not have any effect. The Score remains visible on the screen.
+ *
+ */
+public class Task {
+
+
+
+}
